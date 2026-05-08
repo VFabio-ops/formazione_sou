c16313008666c699d69dafb406102c2f8a572777
VFabio-ops <fabio.viscusi@sourcesense.com>
2026-05-08 16:28:09 +0200

    Changed script comment

--- Esercitazioni/array_v1.sh
+++ Esercitazioni/array_v1.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# printf "%s\n" ${arr[@]} | tr -d [:digit:] | tr -s [:space:] | tr [:lower:] [:upper:] | sort 
+# declare -a serve per dichiarare un array.
+
 declare -a arr=("Pippo" "Franco" "Spugna" "Forte" "Sapone" "Banana" "Zeppa" "Ciabatta" "Pippo")
 
-sortedArr=($(printf "%s\n" "${arr[@]}" | sort -u))
-list="${sortedArr[@]}"
-trans=$(echo $list | tr [:upper:] [:lower:])
+# printf stampa i valori dell'array "%s\n" serve invece per andare a capo.
+# "tr", letteralmente translate, la sua funzione è di sostituzione o rimozione di caratteri in base a quale flag viene utilizzata.
+# "tr -s" serve a comprimere (in questo caso lo spazio) 
+# "tr [:upper:] [:lower:]" rende tutti i caratteri MAIUSCOLI in minuscoli
+# sort -u permette di ordinare alfabeticamente il contenuto dell'array e grazie alla flag (che equivarrebbe a uniq) eliminare i "doppioni"
 
-echo $trans
+
+printf "%s\n" ${arr[@]} | tr -d [:digit:] | tr -s [:space:] | tr [:lower:] [:upper:] | sort -u
