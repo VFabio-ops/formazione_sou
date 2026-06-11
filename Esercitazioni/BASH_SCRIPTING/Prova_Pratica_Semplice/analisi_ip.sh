1558716ef3fcb5321a79523621b6388bfb081c0c
Nasty <nasty@local.dev>
2026-06-11 13:14:32 +0200

    Academy 6 Updated analisi_ip.sh

--- Esercitazioni/BASH_SCRIPTING/Prova_Pratica_Semplice/analisi_ip.sh
+++ Esercitazioni/BASH_SCRIPTING/Prova_Pratica_Semplice/analisi_ip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Usage: ./analisi_ip.sh <example.txt>
-
+# Usage: ./analisi_ip.sh 
+FILE=accessi.txt
 # Questo script serve per analizzare un file di testo contenente vari indirizzi ip,
 # ordinarli e contarli dando in output i 3 indirizzi ip più comuni
 
-sort "$1" | uniq -c | sort -rn | head -3
+sort "$FILE" | uniq -c | sort -rn | head -3
 
 # sort       : ordina gli IP (mette vicini quelli uguali)
 # uniq -c    : conta le occorrenze consecutive
